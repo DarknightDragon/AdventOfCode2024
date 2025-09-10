1598764866d954d744ed7ca22d53d05d1873b0c7
DarknightDragon <11633926+DarknightDragon@users.noreply.github.com>
2025-09-09 23:24:32 -0400

    Completed day 11 part 2

--- Q11/Q11.cpp
+++ Q11/Q11.cpp
@@ -4,9 +4,11 @@
 #include <list> // std::list
 #include <regex>
 #include <cmath> // std::logBASE()
+#include <unordered_map> // std::unordered_map
 
 unsigned long long getNumDigits( unsigned long long n );
-void blink( std::list<unsigned long long>& list, unsigned n = 1 );
+unsigned long long blink( std::list<unsigned long long> list, unsigned n = 1 );
+unsigned long long blink_part2( const std::list<unsigned long long>& list, unsigned n = 1 );
 
 int main() {
 	std::ifstream file( "Data.txt" );
@@ -30,9 +32,8 @@ int main() {
 		list.emplace_back( std::stoull( *it++ ) );
 	}
 
-	blink( list, 75 );
-	std::cout << "There are " << list.size() << " stones" << std::endl;
-	//for ( auto num : list ) { std::cout << num << " "; }
+	std::cout << "Part 1: There are " << blink( list, 25 ) << " stones" << std::endl;
+	std::cout << "Part 2: There are " << blink_part2( list, 75 ) << " stones" << std::endl;
 
 	return 0;
 }
@@ -41,9 +42,9 @@ unsigned long long getNumDigits( unsigned long long n ) {
 	return static_cast<unsigned long long>( std::log10l( n ) ) + 1;
 }
 
-void blink( std::list<unsigned long long>& list, unsigned n ) {
+unsigned long long blink( std::list<unsigned long long> list, unsigned n ) {
 	if ( list.size() == 0 ) {
-		return;
+		return 0;
 	}
 
 	int i = 0;
@@ -54,13 +55,85 @@ void blink( std::list<unsigned long long>& list, unsigned n ) {
 				*listIt = 1ull;
 			}
 			else if ( numDigits % 2 == 0 ) {
-				unsigned int placeHolder = static_cast<unsigned int>( std::pow( 10, ( numDigits / 2 ) ) );
-				list.insert( listIt, static_cast<unsigned long long>( *listIt / placeHolder ) );
-				*listIt = *listIt % placeHolder;
+				unsigned int placeholder = static_cast<unsigned int>( std::pow( 10, ( numDigits / 2 ) ) );
+				list.insert( listIt, static_cast<unsigned long long>( *listIt / placeholder ) );
+				*listIt = *listIt % placeholder;
 			}
 			else {
 				*listIt *= 2024;
 			}
 		}
 	}
+
+	return list.size();
+}
+
+unsigned long long blink_part2( const std::list<unsigned long long>& list, unsigned n ) {
+	unsigned long long total{ 0ull };
+
+	std::unordered_map<unsigned long long, unsigned long long> currMap;
+	std::unordered_map<unsigned long long, unsigned long long> nextMap;
+
+	// fill list into map
+	for ( const auto& num : list ) {
+		if ( currMap.find( num ) == currMap.end() ) {
+			currMap.insert( { num, 1ull } );
+		}
+		else {
+			++currMap.at( num );
+		}
+	}
+
+	for ( int i = 0; i < n; i++ ) {
+		for ( auto num : currMap ) {
+			// rule 1: if 0, then becomes 1
+			if ( num.first == 0 ) {
+				if ( nextMap.find( 1ull ) == nextMap.end() ) {
+					nextMap.insert( { 1ull, num.second } );
+				}
+				else {
+					nextMap.at( 1 ) += num.second;
+				}
+			}
+			// rule 2: if even digits, split into two numbers
+			// C++ 17+ allows initialization in if
+			else if ( auto numDigits = getNumDigits( num.first ); numDigits % 2 == 0 ) {
+				unsigned long long placeholder = static_cast<unsigned long long>( std::pow( 10, numDigits / 2 ) );
+				unsigned long long leftNum = num.first / placeholder;
+				if ( nextMap.find( leftNum ) == nextMap.end() ) {
+					nextMap.insert( { leftNum, num.second } );
+				}
+				else {
+					nextMap.at( leftNum ) += num.second;
+				}
+				unsigned long long rightNum = num.first % placeholder;
+				if ( nextMap.find( rightNum ) == nextMap.end() ) {
+					nextMap.insert( { rightNum, num.second } );
+				}
+				else {
+					nextMap.at( rightNum ) += num.second;
+				}
+
+			}
+			// rule 3: multiply num by 2024
+			else {
+				unsigned long long newNum = num.first * 2024;
+				if ( nextMap.find( newNum ) == nextMap.end() ) {
+					nextMap.insert( { newNum, num.second } );
+				}
+				else {
+					nextMap.at( newNum ) += num.second;
+				}
+			}
+		} // close for each num in currMap
+
+		// next map has the next current list, swap them and clear nextMap for next list
+		currMap.swap( nextMap );
+		nextMap.clear();
+	}
+
+	for ( auto num : currMap ) {
+		total += num.second;
+	}
+	return total;
 }
